cc9d952e3c15cd33489ac5809b6cefbf0c8cda12
Falisz <lukasz.falisz@gmail.com>
2025-08-03 01:34:49 +0200

    Label added

--- frontend/src/components/Users/Index.js
+++ frontend/src/components/Users/Index.js
@@ -1,3 +1,4 @@
+// FRONTEND\components\Users\Index.js
 import React, {useEffect, useState, useMemo, useCallback} from 'react';
 import {useLocation, useNavigate, useParams} from "react-router-dom";
 import axios from 'axios';
