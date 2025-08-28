be74c2de498ef21cc2cf1c3cca46d9786b8045e3
Falisz <lukasz.falisz@gmail.com>
2025-08-28 12:44:23 +0200

    Frontend: Updated user details UI.

--- frontend/src/components/Users/Details.js
+++ frontend/src/components/Users/Details.js
@@ -15,36 +15,37 @@ const UserDetails = ({ user, loading, handleDelete }) => {
     }
 
     return (
-        <>
-            <h1>{user?.first_name + ' ' + user?.last_name}</h1>
-            <div className="user-detail">
-                <div className={"user-detail-label"}>ID</div>
-                <div className={"user-detail-data"}>{user.ID}</div>
-                <div className={"user-detail-label"}>Name</div>
-                <div className={"user-detail-data"}>{user.first_name} {user.last_name}</div>
-                <div className={"user-detail-label"}>Login</div>
-                <div className={"user-detail-data"}>{user.login}</div>
-                <div className={"user-detail-label"}>E-Mail</div>
-                <div className={"user-detail-data"}>{user.email}</div>
-                <div className={"user-detail-label"}>Active?</div>
-                {user.active ? <div className={"user-detail-data true"}><i className="material-symbols-outlined">check</i> Active</div>
-                    : <div className={"user-detail-data false"}><i className="material-symbols-outlined">close</i> Not Active</div>}
-                <div className={"user-detail-label"}>Report to</div>
-                {user.managers.length > 0 ? user.managers.map((manager) => (
-                    <div className={"user-detail-data"} key={manager.ID} onClick={() => navigate('/employees/' + manager.ID)}>{manager.first_name} {manager.last_name}</div>
-                )):<div className={"user-detail-data placeholder"}>No manager assigned.</div>}
-                <div className={"user-detail-label"}>Roles</div>
-                {user.roles.length > 0 ? user.roles.map((role) => (
-                    <div className={"user-detail-data"} key={role.ID}>{role.name}</div>
-                )):<div className={"user-detail-data placeholder"}>Na roles assigned.</div>}
-                <button type="button" className="button" onClick={() => navigate('/employees/edit/' + user.user)}>
-                    <i className={'material-symbols-outlined'}>edit</i> Edit Employee
-                </button>
-                <button type="button" className="delete-button" onClick={handleDelete}>
-                    <i className={'material-symbols-outlined'}>delete</i>  Delete Employee
-                </button>
+        <div className="user-detail">
+            <div className="user-detail-group user-detail-header">
+                <div className={"user-id"} title={"Employee ID"}>#{user.ID}</div>
+                <div className={"user-name"} title={"Full Name"}>{user.first_name} {user.last_name}</div>
             </div>
-        </>
+            <div className="user-detail-group">
+                <div className={"user-detail-label"}>Login details</div>
+                <div className={"user-login"} title={"Login ID"}>Login ID: {user.ID}</div>
+                <div className={"user-login"} title={"Login alias"}>Login alias: {user.login}</div>
+                <div className={"user-login"} title={"Login e-mail"}>Login e-mail: {user.email}</div>
+            </div>
+            <div className={"user-detail-label"}>Status</div>
+            {user.active ? <div className={"user-detail-data true"}><i className="material-symbols-outlined">check</i> Employee's account is active</div>
+                : <div className={"user-detail-data false"}><i className="material-symbols-outlined">close</i> Employee's account is not active</div>}
+            {user.manager_view_access ? <div className={"user-detail-data true"}><i className="material-symbols-outlined">check</i> Employee has access to Manager View</div>
+                : <div className={"user-detail-data false"}><i className="material-symbols-outlined">close</i> Employee does not have access to Manager View </div>}
+            <div className={"user-detail-label"}>Report to</div>
+            {user.managers.length > 0 ? user.managers.map((manager) => (
+                <div className={"user-detail-data"} key={manager.ID} onClick={() => navigate('/employees/' + manager.ID)}>{manager.first_name} {manager.last_name}</div>
+            )):<div className={"user-detail-data placeholder"}>No manager assigned.</div>}
+            <div className={"user-detail-label"}>Roles</div>
+            {user.roles.length > 0 ? user.roles.map((role) => (
+                <div className={"user-detail-data"} key={role.ID}>{role.name}</div>
+            )):<div className={"user-detail-data placeholder"}>Na roles assigned.</div>}
+            <button type="button" className="button" onClick={() => navigate('/employees/edit/' + user.user)}>
+                <i className={'material-symbols-outlined'}>edit</i> Edit Employee
+            </button>
+            <button type="button" className="delete-button" onClick={handleDelete}>
+                <i className={'material-symbols-outlined'}>delete</i>  Delete Employee
+            </button>
+        </div>
     );
 };
 
