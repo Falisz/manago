fa9c8319f9d7cfaa418cfc8d18a696ac82815a4b
Falisz <158840854+Falisz@users.noreply.github.com>
2025-08-29 11:19:57 +0000

    Frontend: Update to Users and Roles details UI.

--- frontend/src/components/Roles/Details.js
+++ frontend/src/components/Roles/Details.js
@@ -2,7 +2,7 @@
 import React, {useEffect} from "react";
 import Loader from "../Loader";
 import useRole from "../../hooks/useRole";
-import {useModals} from "../../contexts/ModalContext";
+import { useModals } from "../../contexts/ModalContext";
 
 const RoleDetails = ({ roleId }) => {
     const { role, loading, fetchRole, deleteRole } = useRole();
@@ -28,38 +28,52 @@ const RoleDetails = ({ roleId }) => {
         return <h1>Role not found!</h1>;
     }
 
+    console.log(role);
+
     return (
-        <>
-            <h1>{role.name}</h1>
-            <div className="role-detail">
-                <div className="role-detail-label">ID</div>
-                <div className="role-detail-data">{role.ID}</div>
-                <div className="role-detail-label">Name</div>
-                <div className="role-detail-data">{role.name}</div>
-                <div className="role-detail-label">Power</div>
-                <div className="role-detail-data">{role.power}</div>
-                <div className="role-detail-label">System Default?</div>
+        <div className="role-detail">
+            <div className="role-detail-header">
+                <div className={"role-id"} title={"Role ID"}>#{role.ID}</div>
+                <div className={"role-name"} title={"Role Name"}>{role.name}</div>
+            </div>
+            <div className="role-detail-group">
+                <div className={"role-detail-label"}>Role details</div>
+                <div className={"role-login"} title={"Power"}>Power: {role.power}</div>
                 {role.system_default ? (
-                    <div className="role-detail-data true">
-                        <i className="material-symbols-outlined">check</i> Yes
+                    <div className="role-system-default">
+                        <i className="material-symbols-outlined true">check</i> This is system default role.
                     </div>
                 ) : (
-                    <div className="role-detail-data false">
-                        <i className="material-symbols-outlined">close</i> No
+                    <div className="role-system-default">
+                        <i className="material-symbols-outlined false">close</i> This is not system default role.
                     </div>
                 )}
-                <button
-                    type="button"
-                    className="button"
-                    onClick={() => openModal({ type: 'roleEdit', data: { id: role.ID } })}
-                >
-                    <i className="material-symbols-outlined">edit</i> Edit Role
-                </button>
-                <button type="button" className="delete-button" onClick={handleDelete}>
-                    <i className="material-symbols-outlined">delete</i> Delete Role
-                </button>
+                </div>
+            
+            <div className="role-detail-group">
+                <div className={"role-detail-label"}>Users with this role</div>
+                {role.users?.length > 0 ? role.users.map((user) => (
+                    <div 
+                        className={"user-detail-data link"} 
+                        key={user.ID} 
+                        onClick={() => openModal({ type: 'userDetails', data: { id: user.ID } })}
+                    >
+                        {user.first_name} {user.last_name}
+                    </div>
+                )) :
+                <div className={"user-detail-data placeholder"}>No users with this role.</div>}
             </div>
-        </>
+            <button
+                type="button"
+                className="button"
+                onClick={() => openModal({ type: 'roleEdit', data: { id: role.ID } })}
+            >
+                <i className="material-symbols-outlined">edit</i> Edit Role
+            </button>
+            <button type="button" className="delete-button" onClick={handleDelete}>
+                <i className="material-symbols-outlined">delete</i> Delete Role
+            </button>
+        </div>
     );
 };
 
--- frontend/src/components/Users/Details.js
+++ frontend/src/components/Users/Details.js
@@ -30,7 +30,7 @@ const UserDetails = ({ userId }) => {
 
     return (
         <div className="user-detail">
-            <div className="user-detail-group user-detail-header">
+            <div className="user-detail-header">
                 <div className={"user-id"} title={"Employee ID"}>#{user.ID}</div>
                 <div className={"user-name"} title={"Full Name"}>{user.first_name} {user.last_name}</div>
             </div>
@@ -40,21 +40,49 @@ const UserDetails = ({ userId }) => {
                 <div className={"user-login"} title={"Login alias"}>Login alias: {user.login}</div>
                 <div className={"user-login"} title={"Login e-mail"}>Login e-mail: {user.email}</div>
             </div>
-            <div className={"user-detail-label"}>Status</div>
-            {user.active ? <div className={"user-detail-data true"}><i className="material-symbols-outlined">check</i> Employee's account is active</div>
-                : <div className={"user-detail-data false"}><i className="material-symbols-outlined">close</i> Employee's account is not active</div>}
-            {user.manager_view_access ? <div className={"user-detail-data true"}><i className="material-symbols-outlined">check</i> Employee has access to Manager View</div>
-                : <div className={"user-detail-data false"}><i className="material-symbols-outlined">close</i> Employee does not have access to Manager View </div>}
-            <div className={"user-detail-label"}>Report to</div>
-            {user.managers.length > 0 ? user.managers.map((manager) => (
-                <div className={"user-detail-data"} key={manager.ID} onClick={() => openModal({ type: 'userDetails', data: { id: manager.ID } })}>
-                    {manager.first_name} {manager.last_name}
-                </div>
-            )):<div className={"user-detail-data placeholder"}>No manager assigned.</div>}
-            <div className={"user-detail-label"}>Roles</div>
-            {user.roles.length > 0 ? user.roles.map((role) => (
-                <div className={"user-detail-data"} key={role.ID}>{role.name}</div>
-            )):<div className={"user-detail-data placeholder"}>Na roles assigned.</div>}
+            <div className="user-detail-group">
+                <div className={"user-detail-label"}>Status</div>
+                {user.active ? 
+                    <div className={"user-detail-data true"}>
+                        <i className="material-symbols-outlined">check</i> Employee's account is active
+                    </div> : 
+                    <div className={"user-detail-data false"}>
+                        <i className="material-symbols-outlined">close</i> Employee's account is not active
+                    </div> }
+                {user.manager_view_access ?
+                    <div className={"user-detail-data true"}>
+                        <i className="material-symbols-outlined">check</i> Employee has access to Manager View
+                    </div> : 
+                    <div className={"user-detail-data false"}>
+                        <i className="material-symbols-outlined">close</i> Employee does not have access to Manager View
+                    </div> }
+            </div>
+            <div className="user-detail-group">
+                <div className={"user-detail-label"}>Reports to</div>
+                {user.managers.length > 0 ? user.managers.map((manager) => (
+                    <div 
+                        className={"user-detail-data link"} 
+                        key={manager.ID} 
+                        onClick={() => openModal({ type: 'userDetails', data: { id: manager.ID } })}
+                    >
+                        {manager.first_name} {manager.last_name}
+                    </div>
+                )) :
+                <div className={"user-detail-data placeholder"}>No manager assigned.</div>}
+            </div>
+            <div className="user-detail-group">
+                <div className={"user-detail-label"}>Roles</div>
+                {user.roles.length > 0 ? user.roles.map((role) => (
+                    <div
+                        className={"user-detail-data link"}
+                        key={role.ID}
+                        onClick={() => openModal({ type: 'roleDetails', data: { id: role.ID } })}
+                    >
+                        {role.name}
+                    </div>
+                )) :
+                <div className={"user-detail-data placeholder"}>Na roles assigned.</div>}
+            </div>
             <button type="button" className="button" onClick={() => openModal({ type: 'userEdit', data: { id: user.ID } })}>
                 <i className={'material-symbols-outlined'}>edit</i> Edit Employee
             </button>
