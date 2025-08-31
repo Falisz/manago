d5b622c0302aed77dd3aabd18bad15ab25d5b2b4
Falisz <lukasz.falisz@gmail.com>
2025-08-31 21:33:54 +0200

    Frontend: Teams initial implementation.

---
+++ frontend/src/components/Teams/Index.js
@@ -0,0 +1,28 @@
+// FRONTEND/components/Teams/Index.js
+import React from "react";
+import { useModals } from "../../contexts/ModalContext";
+import Button from "../Button";
+import InWorks from "../InWorks";
+
+const TeamsIndex = () => {
+    const { openModal } = useModals();
+
+    return (
+        <>
+            <h1>Teams in Zyrah</h1>
+            <Button
+                className="new-user-button"
+                onClick={() => openModal({ type: 'teamNew' })}
+                label={'Add new Team'}
+                icon={'add'}
+            />
+            <InWorks
+                title={'Teams'}
+                icon={'groups'}
+                description={"There will be a table of teams with following columns: team names, codename, managers and team leaders. If the branch and projects modules are enabled they are also gonna be present there."}
+            />
+        </>
+    );
+};
+
+export default TeamsIndex;
