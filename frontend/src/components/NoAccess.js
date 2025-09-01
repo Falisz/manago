1b2a598c13853236a19548999322a55d2a98e2f2
Falisz <158840854+Falisz@users.noreply.github.com>
2025-09-01 14:24:20 +0000

    Frontend: Small change of user to there.

--- frontend/src/components/NoAccess.js
+++ frontend/src/components/NoAccess.js
@@ -8,7 +8,7 @@ export const NoAccess = () => {
     return (
         <div className="app-notice app-no-access">
             <span className="main-icon material-symbols-outlined">error</span>
-            <p>Hi {user?.first_name || 'User'}! Looks like you don't have sufficient permissions to visit this portal.</p>
+            <p>Hi {user?.first_name || 'there'}! Looks like you don't have sufficient permissions to visit this portal.</p>
             <p>You can <Link to={'/logout'}>logout</Link> and switch to another account.</p>
         </div>
     );
