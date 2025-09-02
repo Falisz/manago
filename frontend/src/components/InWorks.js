bb4b9e3756cd7579bb3ee356aed2e27c5ce55fce
Falisz <158840854+Falisz@users.noreply.github.com>
2025-09-02 11:39:56 +0000

    Frontend: Updated InWorks component to close modals if appears in one.

--- frontend/src/components/InWorks.js
+++ frontend/src/components/InWorks.js
@@ -1,8 +1,10 @@
 //FRONTEND/components/InWorks.js
 import {Link, useLocation} from "react-router-dom";
+import {useModals} from "../contexts/ModalContext";
 
-export const InWorks = ({ title, icon, description = null }) => {
+export const InWorks = ({ title, icon, description = null, modal = false }) => {
     const location = useLocation();
+    const { closeTopModal } = useModals();
     return (
         <div className="app-notice app-in-works">
             <span className="main-icon material-symbols-outlined">{icon ? icon : 'manufacturing'}</span>
@@ -11,7 +13,7 @@ export const InWorks = ({ title, icon, description = null }) => {
                 This page is under construction. It will be available soon.
             </p>
             { description && <p style={{ width: '66%' }}>{description}</p> }
-            { location.pathname !== '/' && <Link to="/">Return to Dashboard</Link>}
+            { modal ? (<Link onClick={() => closeTopModal()}>Go back.</Link>) : (location.pathname !== '/' && <Link to="/">Return to Dashboard.</Link>)}
         </div>
     )};
 
