3693e699ef2417d2f0e7f79e7b1b766278f7cd70
Falisz <lukasz.falisz@gmail.com>
2025-09-03 18:52:16 +0200

    Frontend: Small changes to AppModules component's logic.

--- frontend/src/components/AppModules.js
+++ frontend/src/components/AppModules.js
@@ -20,7 +20,6 @@ const AppModules = () => {
                 }, 300);
             },
         });
-
     }
 
     return (
@@ -30,40 +29,36 @@ const AppModules = () => {
                 <br/><b>These settings should be changed only by a system admin.</b>
             </div>
             <div className="modules-list">
-                {appConfig.modules?.length === 0 ? (
-                    <p>No modules found.</p>
-                ) : (
-                    appConfig.modules?.map((module) => {
-                        const isMain = module.id === 0;
-                        return (
-                            <div className="modules-list-row" key={module.id}>
-                                <div className="module-content">
-                                    {module.icon && <span className="module-icon material-symbols-outlined">{module.icon}</span>}
-                                    <div className="module-title">{module.title}</div>
-                                    <label className="module-switch">
-                                        <input
-                                            type="checkbox"
-                                            checked={module.enabled}
-                                            onChange={() => handleToggleConfirm(module.id, module.enabled)}
-                                            disabled={isMain}
-                                        />
-                                        <span className="toggle-slider"></span>
-                                    </label>
-                                </div>
-                                {(
-                                    <div className="module-description">
-                                        {module.description ||
-                                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
-                                            "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud " +
-                                            "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure " +
-                                            "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
-                                            "Excepteur sint occaecat cupidatat non proident."}
-                                    </div>
-                                )}
+                { appConfig.modules?.length > 0 ? (appConfig.modules?.map((module) => {
+                    const isMain = module.id === 0;
+                    return (
+                        <div className="modules-list-row" key={module.id}>
+                            <div className="module-content">
+                                {module.icon && <span className="module-icon material-symbols-outlined">{module.icon}</span>}
+                                <div className="module-title">{module.title}</div>
+                                <label className="module-switch">
+                                    <input
+                                        type="checkbox"
+                                        checked={module.enabled}
+                                        onChange={() => handleToggleConfirm(module.id, module.enabled)}
+                                        disabled={isMain}
+                                    />
+                                    <span className="toggle-slider"></span>
+                                </label>
                             </div>
-                        );
-                    })
-                )}
+                            {(
+                                <div className="module-description">
+                                    {module.description ||
+                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+                                        "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud " +
+                                        "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure " +
+                                        "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                                        "Excepteur sint occaecat cupidatat non proident."}
+                                </div>
+                            )}
+                        </div>
+                    ); }
+                )) : <p>No modules found.</p> }
             </div>
         </>
     );
