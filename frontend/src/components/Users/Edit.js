e823ef2634afd6d82b7e4411ddecbabdf428dc3d
Falisz <lukasz.falisz@gmail.com>
2025-09-18 19:45:09 +0200

    Frontend: Make-shift Edit User assignment

--- frontend/src/components/Users/Edit.js
+++ frontend/src/components/Users/Edit.js
@@ -1,6 +1,6 @@
 // FRONTEND/components/Users/Edit.js
 import React, {useEffect, useState} from 'react';
-import { useModals } from '../../contexts/ModalContext';
+import {useModals} from '../../contexts/ModalContext';
 import useUser from '../../hooks/useUser';
 import useRole from '../../hooks/useRole';
 import Loader from '../Loader';
@@ -19,19 +19,209 @@ const FORM_CLEAN_STATE = {
     manager_view_access: false
 };
 
-export const UserRoleAssignment = ({users}) => {
-    const { closeTopModal } = useModals();
-    const { roles, rolesLoading: loading, fetchRoles } = useRole();
-    const [ formData, setFormData ] = useState({mode: 'add', roleId: null});
+const MultiDropdownGroup = ({
+                                dropdownName,
+                                item,
+                                itemPlural,
+                                dataSource,
+                                getSelectableItems,
+                                handleChange,
+                                formData,
+                                handleRemoveItem,
+                                handleAddItem,
+                                addNewItem
+                            }) =>
+    <div className='form-group'>
+        <div className={'form-label'}>
+            {itemPlural}
+        </div>
+        {dataSource?.length === 0 ? (<p>No {itemPlural.toLowerCase()} available.</p>) : (
+            <>
+                <div className={'form-section'}>
+                    {formData[dropdownName].map((userId, index) => (
+                        <div key={index} className='dropdown-item'>
+                            <Dropdown
+                                name={dropdownName}
+                                value={userId}
+                                options={getSelectableItems(index, dropdownName).map(el => ({
+                                    id: el.id,
+                                    name: dropdownName === 'manager_ids' ?
+                                        el.first_name + ' ' + el.last_name :
+                                        el.name
+                                }))}
+                                onChange={(e) => handleChange(e, index)}
+                                placeholder={`Select ${item}`}
+                                noneAllowed={true}
+                            />
+                            {(index > 0 || formData[dropdownName][0] !== null) && (
+                                <Button
+                                    className={'remove-button'}
+                                    onClick={() => handleRemoveItem(dropdownName, index)}
+                                    icon={'cancel'}
+                                    transparent={true}
+                                />
+                            )}
+                        </div>
+                    ))}
+                </div>
+                <Button
+                    className={'new-dropdown-button'}
+                    onClick={() => handleAddItem(dropdownName)}
+                    icon={'add_circle'}
+                    label={`Add ${item}`}
+                    disabled={!(addNewItem(dropdownName))}
+                    transparent={true}
+                />
+            </>
+        )}
+    </div>;
+
+export const UserRoleBulkAssignment = ({users}) => {
+    const {closeTopModal} = useModals();
+    const {roles, rolesLoading: loading, fetchRoles} = useRole();
+    const [formData, setFormData] = useState({mode: 'add', roleId: null});
 
     useEffect(() => {
         fetchRoles().then();
     }, [fetchRoles]);
 
     const handleChange = (e) => {
-        const { value, name } = e.target;
+        const {value, name} = e.target;
         setFormData(prev => ({...prev, [name]: value}));
     }
+    const handleSubmit = () => {
+        console.warn('User Role assignment feature not implemented yet.');
+        closeTopModal();
+    }
+
+    const header = (formData.mode === 'add' ? 'Assigning Role to ' : 'Removing Role from ') +
+        ( users.length > 1 ?
+            `${users.length} User${users.length > 1 ? 's' : ''}` :
+            users[0].first_name + ' ' + users[0].last_name );
+
+    if (loading) return <Loader/>;
+
+    return <>
+        <h1>
+            {header}
+        </h1>
+        <div className='app-form' style={{minHeight: '100px'}}>
+            {users.length > 1 ? <>
+                <h2 className={'form-header'}>Selected Users</h2>
+                <div>
+                    {users?.map((user, index) =>
+                        (user.first_name + ' ' + user.last_name + (index !== users.length - 1 ? ', ' : '')))
+                    }
+                </div>
+            </> : null
+            }
+            <h2 className={'form-header'}>Role</h2>
+            <div className='form-section'>
+                <Dropdown
+                    name={'mode'}
+                    value={formData.mode}
+                    options={[{id: 'add', name: 'Add'}, {id: 'remove', name: 'Remove'}]}
+                    onChange={handleChange}
+                    placeholder={`Select mode`}
+                    searchable={false}
+                    style={{width: '100px', minWidth: 'unset'}}
+                />
+                <Dropdown
+                    name={'roleId'}
+                    value={formData.roleId}
+                    options={roles?.map(role => ({
+                        id: role.id,
+                        name: role.name
+                    }))}
+                    onChange={handleChange}
+                    placeholder={`Select role`}
+                />
+            </div>
+            <div className='form-section align-center'>
+                <Button
+                    className={'save-button'}
+                    label={((formData.mode === 'remove') ? 'Remove' : 'Assign') + ' Roles' }
+                    icon={'save'}
+                    onClick={handleSubmit}
+                />
+                <Button
+                    className={'discard-button'}
+                    type={'button'}
+                    label={'Discard'}
+                    icon={'close'}
+                    onClick={closeTopModal}
+                />
+            </div>
+        </div>
+    </>
+}
+
+export const UserRoleAssignment = ({user}) => {
+    const {closeTopModal, setDiscardWarning} = useModals();
+    const {roles, rolesLoading: loading, fetchRoles} = useRole();
+    const [formData, setFormData] = useState({role_ids: []});
+
+    useEffect(() => {
+        fetchRoles().then();
+    }, [fetchRoles]);
+
+    useEffect(() => {
+        if (user)
+            setFormData(prev => ({...prev, role_ids: user.roles.map(role => role.id)}));
+    }, [user])
+
+    const getSelectableItems = (index, dropdownName) => {
+        const currentSelected = formData[dropdownName] && formData[dropdownName][index];
+
+        if (dropdownName === 'role_ids') {
+            return roles?.filter(
+                role => (role.id === currentSelected || !formData.role_ids.includes(role.id))
+            ) || [];
+        }
+
+        return [];
+    }
+
+    const addNewItem = (dropdownName) => {
+        return !formData[dropdownName].includes(null) && formData[dropdownName].length < roles.length;
+    };
+
+    const handleChange = (e, index) => {
+        const {value, name} = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: [
+                ...prev[name].slice(0, index),
+                parseInt(value) || null,
+                ...prev[name].slice(index + 1),
+            ],
+        }));
+    }
+
+    const handleAddItem = (field) => {
+        setFormData(prev => ({
+            ...prev,
+            [field]: [...prev[field], null],
+        }))
+    }
+
+    const handleRemoveItem = (field, index) => {
+        if (index !== 0)
+            setFormData(prev => ({
+                ...prev,
+                [field]: prev[field].filter((_, i) => i !== index),
+            }));
+        else
+            setFormData(prev => ({
+                ...prev,
+                [field]: [
+                    null,
+                    ...prev[field].slice(1)
+                ]
+            }));
+        setDiscardWarning(true);
+    };
+
 
     const handleSubmit = () => {
         console.warn('User Role assignment feature not implemented yet.');
@@ -40,54 +230,28 @@ export const UserRoleAssignment = ({users}) => {
 
     if (loading) return <Loader/>;
 
-    const header = (formData.mode === 'add' ? 'Assigning Role to ' : 'Removing Role from ') +
-        (users.length > 1 ? `${users.length} Users` : `${users[0].first_name + ' ' + users[0].last_name}`)
-
-    // If only one user it should have the option like in editForm for MultiDropdown stuff.
-    // Change into two cases entirety of this return instead.
-
-    return <>
-            <h1>
-                {header}
-            </h1>
-            <div className='app-form' style={{minHeight: '100px', padding: '0 0 150px'}}>
-                { users.length > 1 ? <>
-                    <h2 className={'form-header'}>Selected Users</h2>
-                    <div>
-                        { users?.map( (user, index) =>
-                            (user.first_name + ' ' + user.last_name + (index !== users.length - 1  ?  ', ' : '')) )
-                        }
-                    </div>
-                </> : null
-                }
-                <h2 className={'form-header'}>Role</h2>
-                <div className='form-section'>
-                    <Dropdown
-                        name={'mode'}
-                        value={formData.mode}
-                        options={[{id: 'add', name: 'Add'}, {id: 'remove', name: 'Remove'}]}
-                        onChange={handleChange}
-                        placeholder={`Select mode`}
-                        searchable={false}
-                        style={{width: '100px', minWidth: 'unset'}}
-                    />
-                    <Dropdown
-                        name={'roleId'}
-                        value={formData.roleId}
-                        options={roles?.map(role => ({
-                            id: role.id,
-                            name: role.name
-                        }))}
-                        onChange={handleChange}
-                        placeholder={`Select role`}
-                    />
-                </div>
+    if (user) {
+        return <>
+            <h1>Editing Roles of {user.first_name + ' ' + user.last_name}</h1>
+            <form className={'app-form'} onSubmit={handleSubmit}>
+                <MultiDropdownGroup
+                    dropdownName={'role_ids'}
+                    item={'Role'}
+                    itemPlural={'Roles'}
+                    dataSource={roles}
+                    getSelectableItems={getSelectableItems}
+                    handleChange={handleChange}
+                    formData={formData}
+                    handleRemoveItem={handleRemoveItem}
+                    handleAddItem={handleAddItem}
+                    addNewItem={addNewItem}
+                />
                 <div className='form-section align-center'>
                     <Button
                         className={'save-button'}
-                        label={'Assign Role'}
+                        type={'submit'}
+                        label={'Save changes'}
                         icon={'save'}
-                        onClick={handleSubmit}
                     />
                     <Button
                         className={'discard-button'}
@@ -97,17 +261,20 @@ export const UserRoleAssignment = ({users}) => {
                         onClick={closeTopModal}
                     />
                 </div>
-            </div>
-        </>
-    
+            </form>
+        </>;
+    }
+
+
+
 };
 
-const UserEdit = ({ userId, preset }) => {
-    const { loading, error, warning, success, setLoading, fetchUser, saveUser } = useUser();
-    const { users: managers, fetchUsers: fetchManagers } = useUser();
-    const { roles, fetchRoles } = useRole();
-    const { openModal, setDiscardWarning, refreshData, closeTopModal } = useModals();
-    const [ formData, setFormData ] = useState(FORM_CLEAN_STATE);
+const UserEdit = ({userId, preset}) => {
+    const {loading, error, warning, success, setLoading, fetchUser, saveUser} = useUser();
+    const {users: managers, fetchUsers: fetchManagers} = useUser();
+    const {roles, fetchRoles} = useRole();
+    const {openModal, setDiscardWarning, refreshData, closeTopModal} = useModals();
+    const [formData, setFormData] = useState(FORM_CLEAN_STATE);
 
     useEffect(() => {
         fetchRoles().then();
@@ -143,11 +310,11 @@ const UserEdit = ({ userId, preset }) => {
                 manager_view_access: user?.manager_view_access || false,
             });
         });
-        
+
     }, [userId, preset, setLoading, fetchUser, fetchRoles, fetchManagers]);
 
     const handleChange = (e, index) => {
-        const { name, value, type, checked } = e.target;
+        const {name, value, type, checked} = e.target;
 
         if (['role_ids', 'manager_ids'].includes(name)) {
             setFormData(prev => ({
@@ -204,7 +371,7 @@ const UserEdit = ({ userId, preset }) => {
                 closeTopModal();
                 if (!userId) {
                     setTimeout(() => {
-                        openModal({ content: 'userDetails', contentId: savedUser.id });
+                        openModal({content: 'userDetails', contentId: savedUser.id});
                     }, 350);
                 } else {
                     refreshData('user', userId);
@@ -250,61 +417,16 @@ const UserEdit = ({ userId, preset }) => {
         return !formData[dropdownName].includes(null) && formData[dropdownName].length < source.length;
     };
 
-    const MultiDropdownGroup = ({ dropdownName, item, itemPlural, dataSource }) =>
-            <div className='form-group'>
-                <div className={'form-label'}>
-                    {itemPlural}
-                </div>
-                {dataSource?.length === 0 ? (<p>No {itemPlural.toLowerCase()} available.</p>) : (
-                    <>
-                        <div className={'form-section'}>
-                            {formData[dropdownName].map((userId, index) => (
-                                <div key={index} className='dropdown-item'>
-                                    <Dropdown
-                                        name={dropdownName}
-                                        value={userId}
-                                        options={getSelectableItems(index, dropdownName).map(el => ({
-                                            id: el.id,
-                                            name: dropdownName === 'manager_ids' ?
-                                                el.first_name + ' ' + el.last_name :
-                                                el.name
-                                        }))}
-                                        onChange={(e) => handleChange(e, index)}
-                                        placeholder={`Select ${item}`}
-                                        noneAllowed={true}
-                                    />
-                                    {(index > 0 || formData[dropdownName][0] !== null ) && (
-                                        <Button
-                                            className={'remove-button'}
-                                            onClick={() => handleRemoveItem(dropdownName, index)}
-                                            icon={'cancel'}
-                                            transparent={true}
-                                        />
-                                    )}
-                                </div>
-                            ))}
-                        </div>
-                        <Button
-                            className={'new-dropdown-button'}
-                            onClick={() => handleAddItem(dropdownName)}
-                            icon={'add_circle'}
-                            label={`Add ${item}`}
-                            disabled={!(addNewItem(dropdownName))}
-                            transparent={true}
-                        />
-                    </>
-                )}
-            </div>;
 
-    if (loading) return <Loader />;
+    if (loading) return <Loader/>;
 
     if (error) return <div className='error-message'>{error}</div>;
 
     return (
         <>
             <h1>{userId ? 'Edit Employee' : `Add New ${
-                preset === 'manager' ? 'Manager' : 
-                    preset === 'employee' ? 'Employee' : 'User' }`}</h1>
+                preset === 'manager' ? 'Manager' :
+                    preset === 'employee' ? 'Employee' : 'User'}`}</h1>
             {warning && <div className='warning-message'>{warning}</div>}
             {success && <div className='success-message'>{success}</div>}
             <form
@@ -397,12 +519,24 @@ const UserEdit = ({ userId, preset }) => {
                     item={'Role'}
                     itemPlural={'Roles'}
                     dataSource={roles}
+                    handleChange={handleChange}
+                    formData={formData}
+                    handleRemoveItem={handleRemoveItem}
+                    handleAddItem={handleAddItem}
+                    addNewItem={addNewItem}
+                    getSelectableItems={getSelectableItems}
                 />
                 <MultiDropdownGroup
                     dropdownName={'manager_ids'}
                     item={'Manager'}
                     itemPlural={'Managers'}
                     dataSource={managers}
+                    handleChange={handleChange}
+                    formData={formData}
+                    handleRemoveItem={handleRemoveItem}
+                    handleAddItem={handleAddItem}
+                    addNewItem={addNewItem}
+                    getSelectableItems={getSelectableItems}
                 />
                 <div className='form-section align-center'>
                     <Button
