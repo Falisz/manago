5347c19523727a4d1b89138dc7d2aadcc9ff22db
Falisz <lukasz.falisz@gmail.com>
2025-09-21 15:18:20 +0200

    Frontend: Icon selector update - so the browser doesn't complain about custom HTML tag.

--- frontend/src/components/Icon.jsx
+++ frontend/src/components/Icon.jsx
@@ -1,14 +1,14 @@
-// FRONTEND/components/Icon.js
+// FRONTEND/components/Icon.jsx
 import React from 'react';
 
 const Icon = ({i='check_circle', className=null, onClick=null, s=false, clickable=false}) =>
-    <icon
-        className={'material-' + (s ? 'symbols-rounded' : 'icons')
+    <span
+        className={'icon material-' + (s ? 'symbols-rounded' : 'icons')
             + (clickable ? (' app-clickable') : '')
             + (className ? (' ' + className) : '')}
         onClick={onClick}
     >
         {i}
-    </icon>;
+    </span>;
 
 export default Icon;
