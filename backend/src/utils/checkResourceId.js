88bd791f088f5785b6e2d0d16740ec3300775f42
Falisz <158840854+Falisz@users.noreply.github.com>
2025-10-03 11:33:42 +0000

    Backend: Adding new middleware to the utils

---
+++ backend/src/utils/checkResourceId.js
@@ -0,0 +1,23 @@
+// BACKEND/utils/checkResourceId.js
+
+/**
+ * Middleware to check if the ID is provided properly in the parameter.
+ * @param {Request} req
+ * @param {Object} req.params
+ * @param {Response} res
+ * @param {number} res.status
+ * @param {function} res.json
+ * @param {NextFunction} next
+ */
+const checkResourceIdHandler = (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id)
+        return res.status(400).json({ message: 'Resource ID is missing.' });
+
+    if (isNaN(id))
+        return res.status(400).json({ message: `Invalid Resource ID (${id}: ${typeof id}). ID must be a number.` });
+
+    next();
+};
+export default checkResourceIdHandler;
