658af0f13f6851c7703af465d120d62738cc0ca7
Falisz <lukasz.falisz@gmail.com>
2025-10-04 01:05:03 +0200

    Backend: SecurityLog JSDocs added.

--- backend/src/utils/securityLogs.js
+++ backend/src/utils/securityLogs.js
@@ -2,12 +2,14 @@
 import {AppSecurityLog} from "../models/app.js";
 import {now} from "sequelize/lib/utils";
 
+/**
+ * Logs a security-related event to the database.
+ * @param {number} user - The user ID associated with the event.
+ * @param {string} org - The organization associated with the event.
+ * @param {string} action - The action performed by the user.
+ * @param {string} message - Additional details about the event.
+ * @returns {Promise<void>}
+ */
 export async function securityLog(user, org, action, message) {
-    await AppSecurityLog.create({
-        timestamp: now(),
-        user: user,
-        org: org,
-        action: action,
-        message: message
-    });
+    await AppSecurityLog.create({ timestamp: now(), user, org, action, message});
 }
