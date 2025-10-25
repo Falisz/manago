3c714e487f33d640d4185871646a12d3a5417c58
Falisz <lukasz.falisz@gmail.com>
2025-10-25 18:52:11 +0200

    Frontend: WorkPlanner initial styles and idea.

--- frontend/src/components/WorkPlanner/WorkPlanner.jsx
+++ frontend/src/components/WorkPlanner/WorkPlanner.jsx
@@ -1,16 +1,27 @@
 // FRONTEND/components/WorkPlanner/WorkingSchedulesIndex.jsx
 import React from 'react';
 import WorkingSchedulesIndex from './WorkingSchedulesIndex';
+import '../../styles/WorkPlanner.css';
 
 const WorkPlanner = () => {
 
     return (
-        <div>
-            <h1>Your Schedule</h1>
-            <p>Here you can view and edit your schedule.</p>
-            <h1>Your Teams Schedule</h1>
-            <p>Here you can view and edit your teams schedule.</p>
-            <WorkingSchedulesIndex/>
+        <div className={'work-planner app-scroll'}>
+            <div className={'planner-widget'}>
+                <h1>Your Schedule</h1>
+                <p>Here you can view and edit your schedule.</p>
+            </div>
+            <div className={'planner-widget'}>
+                <h1>Your Teams Schedule</h1>
+                <p>Here you can view and edit your teams schedule.</p>
+            </div>
+            <div className={'planner-widget'}>
+                <WorkingSchedulesIndex/>
+            </div>
+            <div className={'planner-widget'}>
+                <h1>Pending Leave approvals</h1>
+                <p>Oncoming leaves to approve or reject...</p>
+            </div>
         </div>
     );
 };
